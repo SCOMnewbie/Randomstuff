3240876582d86877425bfa562a28c2b8eabef61d
Francois.leon <francois.leon@ubisoft.com>
2024-02-19 23:52:51 +0100

    Add scripts to fetch version from Github/Hashicorp products

---
+++ Scripts/Get-LatestGithubRepositoryReleaseVersion.ps1
@@ -0,0 +1,39 @@
+﻿Function Get-LatestGithubRepositoryReleaseVersion {
+    <#
+.SYNOPSIS
+
+Return the latest version of a Github released repository artifact.
+
+.DESCRIPTION
+
+A lot of repositories expose their releases. Github is providing a simple way to fetch the latest released versions. https://docs.github.com/en/repositories/releasing-projects-on-github/linking-to-releases
+
+.PARAMETER RepositoryName
+Specify the repository to use.
+
+.PARAMETER OrganizationName
+Specify the organization to use.
+
+.EXAMPLE
+PS> Get-LatestGithubRepositoryReleaseVersion -OrganizationName aquasecurity -RepositoryName trivy
+
+.EXAMPLE
+PS> Get-LatestGithubRepositoryReleaseVersion -OrganizationName project-copacetic -RepositoryName copacetic
+#>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [String] $OrganizationName,
+        [Parameter(Mandatory)]
+        [String] $RepositoryName
+    )
+    
+    $Regex = '.*tag/v(?<version>.*)'
+    $Url = 'https://github.com/{0}/{1}/releases/latest' -f $OrganizationName.ToLower(),$RepositoryName.ToLower()
+
+    Write-Verbose "[$((Get-Date).TimeOfDay)] Url is: $Url"
+    $FetchUrl = Invoke-WebRequest $Url
+
+    $FetchUrl.BaseResponse.RequestMessage.RequestUri.AbsoluteUri -match $Regex | Out-Null
+    $Matches.version
+}
---
+++ Scripts/Get-LatestStableHashicorpProductVersion.ps1
@@ -0,0 +1,32 @@
+﻿Function Get-LatestStableHashicorpProductVersion {
+<#
+.SYNOPSIS
+
+Return the latest stable version of several Hashicorp products.
+
+.DESCRIPTION
+
+Return the latest stable version of several Hashicorp products.
+
+.PARAMETER Product
+Specify the product you're interrested in.
+
+.EXAMPLE
+PS> Get-LatestStableHashicorpProductVersion -Product vault
+#>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateSet('Terraform','Vault','Consul','Nomad','Packer')]
+        [String] $Product
+    )
+    
+    $Regex = '^\/.*\/(?<version>\d+\.\d+\.\d+)\/.*$'
+    $Url = 'https://releases.hashicorp.com/{0}' -f $Product.ToLower()
+
+    Write-Verbose "[$((Get-Date).TimeOfDay)] Url is: $Url"
+    $FetchUrl = Invoke-WebRequest $Url
+
+    # Select the first 20 links, make sure we select only the stable versions (not beta, alpha...) then catch all versions only and select the first one
+    $FetchUrl.links.href | Select-Object -First 20 | Select-String -Pattern $Regex -AllMatches | ForEach-Object matches | ForEach-Object Groups | Where-Object Name -EQ Version | ForEach-Object value | Select-Object -First 1
+}
