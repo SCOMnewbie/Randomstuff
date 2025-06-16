184681d1ea539035e503c99d85c5df848f71667e
scomnewbie <francois.leon@outlook.com>
2025-06-16 22:31:31 +0200

    Update WebServerV3.ps1

--- WebServer/WebServerV3.ps1
+++ WebServer/WebServerV3.ps1
@@ -1,6 +1,8 @@
 [CmdletBinding()]
 param(
-    [string[]]$ListenerPrefix = @('http://localhost:6161/')
+    [string[]]$ListenerPrefix = @('http://localhost:6161/'),
+    $TenantId = 'e192cada-a04d-4cfc-8b90-d14338b2c7ec', # Azure Tenant Id
+    $Audience = 'api://fca4cdf3-031d-41a2-a0be-ecd854b2f201' # Each application has a singla audience
 )
 
 # If we're running in a container and the listener prefix is not http://*:80/,
@@ -9,10 +11,8 @@ if ($env:IN_CONTAINER -and $listenerPrefix -ne 'http://*:80/') {
     $listenerPrefix = 'http://*:80/'
 }
 
-Write-verbose "listenerPrefix: $listenerPrefix"
-
 function Invoke-Middleware {
-    param($context,$Event)
+    param($context, $Event)
     
     # Log request details
     Write-Host "Received request: $($context.Request.HttpMethod) $($context.Request.Url)"
@@ -22,27 +22,22 @@ function Invoke-Middleware {
     $UrlWithPort = $matches["urlwithport"]
     $Route = $matches["route"] # Is null for /
     $parameters = $matches["parameters"] # Can be for now /blah/blih/bloh
-
-    Write-Host "UrlWithport: $UrlWithPort"
-    Write-Host "Route: $Route"
-    Write-Host "parameters: $parameters"
-
     $IsAdminRequired = $false
-    $AdminRole = 'blah'
+    $AdminRole = 'NoAuthZ'
 
     #Validate valide route
     switch ($Route) {
         '' {
             Write-host "Route / has been request" -ForegroundColor Yellow
         }
-        'blah'{
+        'blah' {
             Write-host "Route /blah has been request" -ForegroundColor Yellow
         }
-        'admin'{
+        'admin' {
             $IsAdminRequired = $true
             $AdminRole = 'SuperAdmin'
             Write-host "Route /admin has been request" -ForegroundColor Yellow
-            Write-host "let's add authentication mechanism for this route" -ForegroundColor Yellow
+            break
         }
         Default {
             Write-host "$Route is not an allowed route" -ForegroundColor Red
@@ -54,15 +49,54 @@ function Invoke-Middleware {
         }
     }
 
-    if($IsAdminRequired){
-        switch ($AdminRole) {
-            'SuperAdmin' { 
-                #Test-AADJWTSignature -Token $AccessToken -TenantId "<my tenantid>"
-             }
-            Default {
+    if ($IsAdminRequired) {
+        $AuthorizationHeader = $context.Request.Headers['Authorization']
+        # Test signature first will always return true or false. False means this is a forged token not an official one.
+        if (Test-AADJWTSignature -Token $AuthorizationHeader -TenantId $TenanId) {
+            $DecodedToken = ConvertFrom-Jwt -Token $AuthorizationHeader
+            # This token is not for another app
+            if ($DecodedToken.Tokenpayload.aud -ne $Audience) {
+                $Context.Response.StatusCode = 403
+                $Context.Response.Close()
+                # Remove unwanted events
+                $Event | Remove-Event
+                break
+            }
 
+            switch ($AdminRole) {
+                'SuperAdmin' {
+                    try {
+                        if ('SuperAdmin' -notin $DecodedToken.TokenPayload.roles) {
+                            # If role is not in the list drop
+                            $Context.Response.StatusCode = 403
+                            $Context.Response.Close()
+                            # Remove unwanted events
+                            $Event | Remove-Event
+                            break
+                        }
+                    }
+                    catch {
+                        $_.Exception.Message
+                    }
+                    
+                    write-host "Authz : $AuthZ"
+                }
+                Default {
+    
+                }
             }
         }
+        else {
+            Write-host "Forged token is not allowed" -ForegroundColor Red
+            $Context.Response.StatusCode = 404
+            $Context.Response.Close()
+            # Remove unwanted events
+            $Event | Remove-Event
+            break
+        }
+    }
+    else {
+        Write-host "Route does not require AuthN/AuthZ" -ForegroundColor Red
     }
 
     return $context
@@ -80,40 +114,40 @@ if (-not $global:HttpListener) {
     }
 }
 
-if($global:HttpListener.Prefixes -gt 1){
+if ($global:HttpListener.Prefixes -gt 1) {
     $Jobname = $global:HttpListener.Prefixes[0]
 }
-else{
+else {
     $Jobname = $global:HttpListener.Prefixes
 }
 
 # Start the listener.
 try { $Httplistener.Start() }
 # If the listener cannot start, write a warning and return.
-catch { Write-Warning "Could not start listener: $_" ;return }
+catch { Write-Warning "Could not start listener: $_" ; return }
 
 Write-verbose "Jobname: $Jobname"
 Write-verbose "HttpListener: $HttpListener"
 
 # The ServerJob will start the HttpListener and listen for incoming requests.
-$script:ServerJob = Start-ThreadJob -ScriptBlock{
+$script:ServerJob = Start-ThreadJob -ScriptBlock {
     param($MainRunspace, $Httplistener, $SourceIdentifier = 'http')
-    while($Httplistener.IsListening){
+    while ($Httplistener.IsListening) {
         $ContextAsync = $Httplistener.GetContextAsync()
-        while(-not $ContextAsync.IsCompleted -or $ContextAsync.IsFaulted -or $ContextAsync.IsCanceled){}
-        if($ContextAsync.IsFaulted){
+        while (-not $ContextAsync.IsCompleted -or $ContextAsync.IsFaulted -or $ContextAsync.IsCanceled) {}
+        if ($ContextAsync.IsFaulted) {
             Write-Error -Exception $ContextAsync.Exception -Category ProtocolError
             continue
         }
-        $Context = $(try {$ContextAsync.Result}catch{$_})
+        $Context = $(try { $ContextAsync.Result }catch { $_ })
 
         $MainRunspace.Events.GenerateEvent(
-            $SourceIdentifier, $Httplistener, @($Context,$Context.Request,$Context.Response),
-            [ordered]@{Url = $Context.Request.Url;Context = $Context; Request = $Context.Request; Response = $Context.Response}
+            $SourceIdentifier, $Httplistener, @($Context, $Context.Request, $Context.Response),
+            [ordered]@{Url = $Context.Request.Url; Context = $Context; Request = $Context.Request; Response = $Context.Response }
         )
     }
 } -Name $Jobname -ArgumentList ([runspace]::DefaultRunspace, $Httplistener) -ThrottleLimit 100 | 
-Add-Member -NotePropertyMembers ([Ordered]@{HttpListener = $Httplistener}) -PassThru
+Add-Member -NotePropertyMembers ([Ordered]@{HttpListener = $Httplistener }) -PassThru
 
 Write-host "Now serving $Jobname" -ForegroundColor Green
 
@@ -127,16 +161,16 @@ $ServerJob | Add-Member -MemberType NoteProperty Created -Value ([DateTime]::now
 
 $Rng = [System.Random]::new()
 
-while($Httplistener.IsListening){
-    foreach($Event in @(Get-Event HTTP*)){
-        $Context,$Request,$Response = $Event.SourceArgs
+while ($Httplistener.IsListening) {
+    foreach ($Event in @(Get-Event HTTP*)) {
+        $Context, $Request, $Response = $Event.SourceArgs
 
         $context = Invoke-Middleware -context $context -Event $Event
 
         #If not processed, let's keep it into the buffer
-        if(-not $Response.OutputStream){continue}
+        if (-not $Response.OutputStream) { continue }
 
-        $Response.Close($OutputEncoding.GetBytes("$($Rng.Next())"),$false)
+        $Response.Close($OutputEncoding.GetBytes("$($Rng.Next())"), $false)
         Write-host "Responded to $($Request.Url) in $([datetime]::Now - $Event.TimeGenerated)" -ForegroundColor Cyan
         $Event | Remove-Event
     }
