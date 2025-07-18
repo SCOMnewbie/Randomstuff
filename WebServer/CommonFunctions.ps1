789b8d466fa8af5dc3f7c2d4115cdffb730b1000
scomnewbie <francois.leon@outlook.com>
2025-07-18 22:06:51 +0200

    -

--- WebServer/CommonFunctions.ps1
+++ WebServer/CommonFunctions.ps1
@@ -27,3 +27,114 @@ function Unauthorized {
     $Event | Remove-Event
     break
 }
+
+function Write-RequestLog {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$LogFilePath,
+        $Context,
+        [Parameter()]
+        [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
+        [string]$HostColor = 'White'
+    )
+
+    begin {
+        $mutexName = "Global\Request" + ($LogFilePath -replace '[\\/:]', '_')
+        $mutex = New-Object System.Threading.Mutex($false, $mutexName)
+    }
+
+    process {
+        if ($mutex.WaitOne(10000)) {
+            try {
+
+                $logDir = Split-Path $LogFilePath
+                if (!(Test-Path $logDir)) {
+                    Write-Verbose "Folder doesn't exist, let's create it"
+                    New-Item -ItemType Directory -Path $logDir -Force | Out-Null
+                }
+
+                if (!(Test-Path $LogFilePath)) {
+                    #File does not exist, create header
+                    Write-Verbose "File doesn't exist, let's create it with header"
+                    "##Fields: date time s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken" | Out-File -FilePath $LogFilePath -Force -Encoding utf8
+                }
+
+                ##Fields: date,time,s-ip,cs-method,cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken
+                $LogEntry = "{0},{1},{2},{3},{4}" -f $(get-date -Format yyyy-MM-dd),$(get-date -Format HH:mm:ss),$context.Request.UserHostAddress,$context.Request.HttpMethod,$Context.Request.RawUrl
+
+                Add-Content -Path $LogFilePath -Value $LogEntry
+                Write-Host $logEntry -ForegroundColor $HostColor
+            }
+            finally {
+                $mutex.ReleaseMutex()
+            }
+        }
+        else {
+            Write-Warning "Could not acquire mutex for $LogFilePath. Log message was skipped."
+        }
+    }
+
+    end {
+        $mutex.Dispose()
+    }
+}
+
+function Write-BackendLog {
+    <#
+        This function is used to log stuff on the console and on disk. This logfile focus on backend logs.
+        Write-BackendLog -LogFilePath $BackendFilePath -LogLevel ERROR -HostColor Red -Message "This is an impoertant error"
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$LogFilePath,
+        [Parameter()]
+        [ValidateSet("INFO", "WARNING", "ERROR", "DEBUG")]
+        [string]$LogLevel = "INFO",
+        [Parameter()]
+        [ValidateSet("Black", "DarkBlue", "DarkGreen", "DarkCyan", "DarkRed", "DarkMagenta", "DarkYellow", "Gray", "DarkGray", "Blue", "Green", "Cyan", "Red", "Magenta", "Yellow", "White")]
+        [string]$HostColor = 'White',
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message
+    )
+
+    begin {
+        $mutexName = "Global\Backend" + ($LogFilePath -replace '[\\/:]', '_')
+        $mutex = New-Object System.Threading.Mutex($false, $mutexName)
+    }
+
+    process {
+        if ($mutex.WaitOne(10000)) {
+            try {
+
+                $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                $logEntry = "$timestamp [$LogLevel] - $Message"
+
+                $logDir = Split-Path $LogFilePath
+                if (!(Test-Path $logDir)) {
+                    Write-Verbose "Folder doesn't exist, let's create it"
+                    New-Item -ItemType Directory -Path $logDir -Force | Out-Null
+                }
+
+                Add-Content -Path $LogFilePath -Value $LogEntry
+                Write-Host $logEntry -ForegroundColor $HostColor
+            }
+            finally {
+                $mutex.ReleaseMutex()
+            }
+        }
+        else {
+            Write-Warning "Could not acquire mutex for $LogFilePath. Log message was skipped."
+        }
+    }
+
+    end {
+        $mutex.Dispose()
+    }
+}
+
+
--- WebServer/WebServerV3.ps1
+++ WebServer/WebServerV3.ps1
@@ -5,18 +5,27 @@
 param(
     [string[]]$ListenerPrefix = @('http://localhost:6161/'),
     $TenantId = 'e192cada-a04d-4cfc-8b90-d14338b2c7ec', # Azure Tenant Id
-    $Audience = 'api://fca4cdf3-031d-41a2-a0be-ecd854b2f201' # Each application has a single audience
+    $Audience = 'api://fca4cdf3-031d-41a2-a0be-ecd854b2f201', # Each application has a single audience
+    $LogFolderName = 'myapi' # This is where we will store the logs under a temp folder
 )
 
 # Load function in memory
 . $PSScriptRoot\CommonFunctions.ps1
 . $PSScriptRoot\useexternalfunction.ps1
 
+$BackendLogFileName = "Backend_{0}.log" -f $(Get-Date -Format "yyyy-MM-dd")
+$RequestLogFileName = "Request_{0}.log" -f $(Get-Date -Format "yyyy-MM-dd")
+$BackendLogFilePath = Join-Path $([System.IO.Path]::GetTempPath()) $LogFolderName $BackendLogFileName
+$RequestLogFilePath = Join-Path $([System.IO.Path]::GetTempPath()) $LogFolderName $RequestLogFileName
+
+Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Green -Message "All logs will be generated in $BackendLogFilePath file for backend logs and $RequestLogFilePath for request logs"
+
+
 function Invoke-Middleware {
     param($context, $Event)
     
     # Log request details
-    Write-Host "Received request: $($context.Request.HttpMethod) $($context.Request.Url)"
+    Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor White -Message "Received request: $($context.Request.HttpMethod) $($context.Request.Url)"
 
     $AllowedURLRegex = '^(?<urlwithport>https?:\/\/[^\s\/]+:\d+)\/(?<route>[^\s\/?]*)\/{0,1}(?<parameters>[^\s?]*)$'
     #Get URL information
@@ -31,11 +40,11 @@ function Invoke-Middleware {
     # This is where you defined if you need AuthZ. If you need admin role, make sure you set $IsAdminRequired to $true and define $AdminRole with the proper role.
     switch ($Route) {
         '' {
-            Write-host "[middleware] Call / function Admin role not required" -ForegroundColor Yellow
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Yellow -Message "[middleware] Call / function Admin role not required"
             break
         }
         'useexternalfunction' {
-            Write-host "[middleware] Call /useexternalfunction function Admin role not required" -ForegroundColor Yellow
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Yellow -Message "[middleware] Call /useexternalfunction function Admin role not required"
             break
         }
         'admin' {
@@ -43,7 +52,7 @@ function Invoke-Middleware {
             $IsAdminRequired = $true
             # This is the role claim we're waiting for in the token
             $AdminRole = 'SuperAdmin'
-            Write-host "[middleware] Call /admin function SuperAdmin role is required" -ForegroundColor Yellow
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Yellow -Message "[middleware] Call /admin function $AdminRole role is required"
             break
         }
         'fakeAdmin' {
@@ -51,11 +60,11 @@ function Invoke-Middleware {
             $IsAdminRequired = $true
             # This is the role claim we're waiting for in the token. For demo purpose, the role does not exist in the application.
             $AdminRole = 'FakeSuperAdmin'
-            Write-host "[middleware] Call /fakeAdmin function FakeSuperAdmin role is required" -ForegroundColor Yellow
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Yellow -Message "[middleware] Call /fakeAdmin function $AdminRole role is required"
             break
         }
         Default {
-            Write-host "[middleware] $Route is not an allowed route" -ForegroundColor Red
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel WARNING -HostColor Red -Message "[middleware] $Route is not an allowed route"
             NotFound -context $context -Event $Event
         }
     }
@@ -83,13 +92,13 @@ function Invoke-Middleware {
                     }
                 }
                 Default {
-                    Write-host "Admin role $AdminRole is not defined in the API make sure to proper configure your API" -ForegroundColor Red
+                    Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel ERROR -HostColor Red -Message "Admin role $AdminRole is not defined in the API make sure to proper configure your API"
                     Forbidden -context $context -Event $Event
                 }
             }
         }
         else {
-            Write-host "Forged token is not allowed" -ForegroundColor Red
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel WARNING -HostColor Red -Message "Forged token is not allowed"
             Unauthorized -context $context -Event $Event
         }
     }
@@ -100,7 +109,6 @@ function Invoke-Middleware {
         Route       = $Route
         Parameters  = $Parameters
     }
-    #return $context
 }
 
 # If we do not have a global HttpListener object
@@ -125,7 +133,7 @@ else {
 # Start the listener.
 try { $Httplistener.Start() }
 # If the listener cannot start, write a warning and return.
-catch { Write-Warning "Could not start listener: $_" ; return }
+catch { Write-BackendLog -LogFilePath $BackendFilePath -LogLevel ERROR -HostColor Red -Message "Could not start listener: $_" ; return} 
 
 # The ServerJob will start the HttpListener and listen for incoming requests.
 $script:ServerJob = Start-ThreadJob -ScriptBlock {
@@ -147,7 +155,8 @@ $script:ServerJob = Start-ThreadJob -ScriptBlock {
 } -Name $Jobname -ArgumentList ([runspace]::DefaultRunspace, $Httplistener) -ThrottleLimit 100 | 
 Add-Member -NotePropertyMembers ([Ordered]@{HttpListener = $Httplistener }) -PassThru
 
-Write-host "Now serving $Jobname" -ForegroundColor Green
+#Write-host "Now serving $Jobname" -ForegroundColor Green
+Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Green -Message "Now serving $Jobname"
 
 # Keep track of the creation time of the ServerJob.
 $ServerJob | Add-Member -MemberType NoteProperty Created -Value ([DateTime]::now) -Force
@@ -204,21 +213,197 @@ while ($Httplistener.IsListening) {
 
         # If parameter is not validated, skip this step
         if (-not $ShouldExit) {
-            Write-host "Responded to $($Request.Url) in $([datetime]::Now - $Event.TimeGenerated)" -ForegroundColor Cyan
+            #Write-host "Responded to $($Request.Url) in $([datetime]::Now - $Event.TimeGenerated)" -ForegroundColor Cyan
+            Write-BackendLog -LogFilePath $BackendLogFilePath -LogLevel INFO -HostColor Cyan -Message "Responded to $($Request.Url) in $([datetime]::Now - $Event.TimeGenerated)"
             $Event | Remove-Event
         } 
     }
-
-    <#
-    # Wait for the PowerShell.Exiting event.
-    $exiting = Wait-Event -SourceIdentifier PowerShell.Exiting -Timeout (Get-Random -Minimum 1 -Maximum 5)
-    if ($exiting) {
-        # If the Stop the web server is still running, stop it.
-        $Jobname | Stop-Job
-        # and break out of the loop.
-        break
-    }
-    #>
 }
 
 # Measure-Command -Expression{ (0..1000).ForEach({irm http://localhost:6161/})}
+
+<#
+{
+  "timestamp": "2025-07-15T21:34:00Z",
+  "level": "INFO",
+  "message": "User logged in",
+  "method": "POST",
+  "path": "/api/login",
+  "status": 200,
+  "ip": "192.168.0.1",
+  "userAgent": "Mozilla/5.0",
+  "durationMs": 128,
+  "authUser": "jdoe",
+  "requestId": "abc123"
+#>
+
+# Set-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name "logExtFileFlags" -value "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,ProtocolVersion,Host,HttpSubStatus"
+
+
+<#
+Date (date) : La date de la requête en temps UTC (cf. point suivant)
+Time (time) : L’heure requête au format UTC. Il n’y a aucun moyen de forcer un horodatage en heure locale (La rotation des logs à minuit heure locale est toutefois possible). Pensez bien au décalage en analysant vos logs IIS (H+2 en été / H+1 en hiver pour la France).
+Client IP Address (c-ip) : L’adresse IP du client. En réalité plutôt l’adresse IP de l’équipement ayant émis la requête. Dans le cadre d’un environnement avec repartition de charge vous risquez de voir uniquement les adresses IP de vos load-balancers. Il existe des moyens de relayer la veritable adresse IP des clients comme ici et ici.
+User Name (cs-username) : L’utilisateur authentifié lorsque cela est possible. Un « – » signifie généralement une authentification anonyme.
+Service Name (s-sitename) : L’identifiant du site ayant pris en charge la requête (1 pour le « Default Web Site » puis de manière séquentielle pour les autres – Il est toutefois possible de changer l’identifiant d’un site de manière programmatique).
+Server Name (s-computername) : Le nom du serveur ayant pris en charge la requête. Si vous analysez les logs d’un site hébergé dans une ferme de serveurs, cette information est capitale.
+Server IP (s-ip) : L’adresse IP ayant pris en charge la requête. En effet un site web peut écouter sur plusieurs adresses IP.
+Server Port (s-port) : Le port d’écoute qui a pris en charge la requête. En effet un site web peut écouter sur plusieurs ports.
+Method (cs-method) : Le « verbe » HTTP associé à la requête.
+URI Stem (cs-uri-stem) : L’URI demandée sans le nom de domaine (/images/logo.png pour l’URI http://www.contoso.com/images/logo.png)
+URI Query (cs-uri-query) : La query string si applicable (param1=value1&param2=value2 pour l’URI http://www.contoso.com/forms/default.aspx?param1=value1&param2=value2)
+Protocol Status (sc-status) : Le code statut de la réponse (« 200 » indiquant que tout va bien)
+Protocol Substatus (sc-substatus) : Le code de sous-statut de la réponse (2 dans la réponse « 401.2« )
+Win32 Status (sc-win32-status) : Le statut Windows de la réponse (Statut propre à Windows)
+Bytes Sent (sc-bytes) : La volumétrie sortante en octets. Information cruciale pour évaluer les gains en termes de compression(s) statique/dynamique et les effets des niveau de compression (allant de 0 à 10 – Ne jamais dépasser 9, en effet le gain entre 9 et 10 est minime en comparaison de la surcharge CPU engendrée. Une petite lecture intéressante est disponible ici. – Moyen mnémotechnique : sc-bytes – SC pour Server to Client)
+Bytes Received (cs-bytes) : La volumétrie entrante (taille de la requête – Moyen mnémotechnique : cs-bytes – CS pour Client to Server)
+Time Taken (time-taken) : Le temps de traitement de la requête (en millisecondes) dans sa totalité : incluant le temps d’attente dans la file d’attente HTTP.sys, le temps de traitement par les serveurs Middle-Office, Back-Office (si applicable) et le temps réseau entre les équipements. Le compteur ne s’arrêtera que lorsque le client acquittera la trame réseau (« ACK ») comme précisé ici. Ce qui implique qu’un time-taken élevé n’est pas forcément un problème au niveau du serveur IIS (les causes peuvent être multiples : latence réseau, requêtes SQL non optimisées …)
+Procol Version (cs-version) : La version du protocole HTTP utilisée : 0.9, 1.0, 1.1, 2.0 …
+Host (cs-host) : Le host header name (si applicable). Un host header name est utilisé pour différencier deux sites qui écoutent sur le même couple IP/Port de manière à router le trafic sur le bon site. En effet le host-header name doit correspondre à l’entrée DNS demandait par les clients pour chaque site. Si on a deux sites correspondant aux entrée DNS www.contoso.com et www.northwindtraders.com écoutant sur le même couple IP:Port, il est necessaire de valoriser le host header name de chaque site avec la valeur DNS associée (www.contoso.com ou www.northwindtraders.com) de manière à être sûr que le trafic est routé sur le bon site.
+User Agent(cs(User-Agent)) : La chaîne d’identification du navigateur / client
+Cookie (cs(Cookie)) : Le cookie reçu ou envoyé associé à la requête.
+Referer(cs(Referer)) : La page où l’on a cliqué précédemment pour arriver à cette page.
+#>
+
+##Fields: date time s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken
+#2025-07-15 21:34:00 192.168.1.10 GET /index.html - 80 - 203.0.113.45 Mozilla/5.0+(Windows+NT+10.0;+Win64;+x64) - 200 0 0 123
+
+<#
+$context
+
+{
+  "Request": {
+    "AcceptTypes": null,
+    "UserLanguages": null,
+    "Cookies": [],
+    "ContentEncoding": {
+      "Preamble": null,
+      "BodyName": "utf-8",
+      "EncodingName": "Unicode (UTF-8)",
+      "HeaderName": "utf-8",
+      "WebName": "utf-8",
+      "WindowsCodePage": 1200,
+      "IsBrowserDisplay": true,
+      "IsBrowserSave": true,
+      "IsMailNewsDisplay": true,
+      "IsMailNewsSave": true,
+      "IsSingleByte": false,
+      "EncoderFallback": {
+        "DefaultString": "�",
+        "MaxCharCount": 1
+      },
+      "DecoderFallback": {
+        "DefaultString": "�",
+        "MaxCharCount": 1
+      },
+      "IsReadOnly": true,
+      "CodePage": 65001
+    },
+    "ContentType": null,
+    "IsLocal": true,
+    "IsWebSocketRequest": false,
+    "KeepAlive": true,
+    "QueryString": [],
+    "RawUrl": "/admin/54325345",
+    "UserAgent": "Mozilla/5.0 (Windows NT 10.0; Microsoft Windows 10.0.26100; fr-FR) PowerShell/7.5.1",
+    "UserHostAddress": "[::1]:6161",
+    "UserHostName": "localhost:6161",
+    "UrlReferrer": null,
+    "Url": "http://localhost:6161/admin/54325345",
+    "ProtocolVersion": {
+      "Major": 1,
+      "Minor": 1,
+      "Build": -1,
+      "Revision": -1,
+      "MajorRevision": -1,
+      "MinorRevision": -1
+    },
+    "ClientCertificateError": null,
+    "RequestTraceIdentifier": "00000000-0000-0000-d800-00400c0000ff",
+    "ContentLength64": 0,
+    "Headers": [
+      "Accept-Encoding",
+      "Authorization",
+      "Host",
+      "User-Agent"
+    ],
+    "HttpMethod": "GET",
+    "InputStream": {
+      "CanRead": true,
+      "CanWrite": true,
+      "CanSeek": true,
+      "Length": 0,
+      "Position": 0,
+      "CanTimeout": false,
+      "ReadTimeout": null,
+      "WriteTimeout": null
+    },
+    "IsAuthenticated": false,
+    "IsSecureConnection": false,
+    "ServiceName": null,
+    "TransportContext": {},
+    "HasEntityBody": false,
+    "RemoteEndPoint": {
+      "AddressFamily": 23,
+      "Address": {
+        "AddressFamily": 23,
+        "ScopeId": 0,
+        "IsIPv6Multicast": false,
+        "IsIPv6LinkLocal": false,
+        "IsIPv6SiteLocal": false,
+        "IsIPv6Teredo": false,
+        "IsIPv6UniqueLocal": false,
+        "IsIPv4MappedToIPv6": false,
+        "Address": null
+      },
+      "Port": 57556
+    },
+    "LocalEndPoint": {
+      "AddressFamily": 23,
+      "Address": {
+        "AddressFamily": 23,
+        "ScopeId": 0,
+        "IsIPv6Multicast": false,
+        "IsIPv6LinkLocal": false,
+        "IsIPv6SiteLocal": false,
+        "IsIPv6Teredo": false,
+        "IsIPv6UniqueLocal": false,
+        "IsIPv4MappedToIPv6": false,
+        "Address": null
+      },
+      "Port": 6161
+    }
+  },
+  "User": null,
+  "Response": {
+    "Headers": [],
+    "ContentEncoding": null,
+    "ContentType": null,
+    "SendChunked": false,
+    "ContentLength64": 0,
+    "Cookies": [],
+    "KeepAlive": true,
+    "OutputStream": {
+      "CanRead": false,
+      "CanSeek": false,
+      "CanWrite": true,
+      "Length": null,
+      "Position": null,
+      "CanTimeout": false,
+      "ReadTimeout": null,
+      "WriteTimeout": null
+    },
+    "RedirectLocation": null,
+    "StatusDescription": "OK",
+    "StatusCode": 200,
+    "ProtocolVersion": {
+      "Major": 1,
+      "Minor": 1,
+      "Build": -1,
+      "Revision": -1,
+      "MajorRevision": -1,
+      "MinorRevision": -1
+    }
+  }
+}
+#>
